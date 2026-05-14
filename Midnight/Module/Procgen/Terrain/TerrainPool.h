dc51e84bd135552e8643683830d745947a639fda
Yoshi <yoshidesigns@outlook.com>
2026-05-13 20:34:43 -0400

    npool

--- Midnight/Module/Procgen/Terrain/TerrainPool.h
+++ Midnight/Module/Procgen/Terrain/TerrainPool.h
@@ -6,55 +6,8 @@
 
 namespace procgen {
 
-	/*
-	* Terrain Pool notes:
-	* ChunkRecord* rec = &pool->slots_[i].record; // Try not to do this - always prefer a handle
-	*/
-	
-	//struct ChunkSlot
-	//{
-	//	ChunkRecord2 record{};
-	//	uint32_t generation{0};
-	//	bool active{false};
-
-	//	TerrainRuntimeState state{ TerrainRuntimeState::Unrequested };
-
-	//	TerrainRenderable renderable;
-	//	TerrainGpuChunk gpu;
-	//};
-	//
-	//struct ChunkHandle {
-	//	uint32_t index;
-	//	bool active;
-	//	uint32_t generation;
-	//};
-	//
-
 	inline void InitTerrainPool(TerrainPool* pool) {
 		
 	}
 	
-	//inline ChunkHandle AddTerrain(TerrainPool* pool) {
-	//	pool->slots_[pool->nActive].generation = 1;
-	//	pool->slots_[pool->nActive].active = true;
-	//	return { pool->nActive++, true };
-
-	//}
-	//
-	//inline void RemoveTerrain(TerrainPool* pool, ChunkHandle handle) {
-	//	if (pool == nullptr) return;
-
-	//	uint32_t last_index = pool->nActive - 1;
-
-	//	// swap
-	//	pool->slots_[handle.index].record = pool->slots_[last_index].record;
-
-	//	// pop
-	//	pool->nActive--;
-
-	//	pool->slots_[handle.index].generation++;
-	//	handle.generation++;
-
-	//	return;
-	//}
 }
